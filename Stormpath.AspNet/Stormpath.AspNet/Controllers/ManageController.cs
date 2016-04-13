99f1ac474e8609a3f5dd4d5279995164bdfdcfcd
Nate Barbettini <nathanaelb@gmail.com>
2016-04-13 15:46:33 -0700

    Semi-working demo

---
+++ Stormpath.AspNet/Stormpath.AspNet/Controllers/ManageController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Stormpath.AspNet.Controllers
+{
+    [Authorize]
+    public class ManageController : Controller
+    {
+        // Stormpath Client can be injected using DI
+        //[FromServices]
+        //public IClient StormpathClient { get; set; }
+
+        //[FromServices]
+        //public IAccount StormpathUser { get; set; }
+
+        // GET: /<controller>/
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

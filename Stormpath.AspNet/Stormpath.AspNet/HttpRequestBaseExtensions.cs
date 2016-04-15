97356fd4f84f2e8684e3dd032284872b6046e0c0
Nate Barbettini <nathanaelb@gmail.com>
2016-04-14 18:42:19 -0700

    Add convenience extensions et cetera

---
+++ Stormpath.AspNet/Stormpath.AspNet/HttpRequestBaseExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using Stormpath.Owin.Abstractions;
+using Stormpath.SDK.Account;
+using Stormpath.SDK.Client;
+
+namespace Stormpath.AspNet
+{
+    public static class HttpRequestBaseExtensions
+    {
+        public static IClient GetStormpathClient(this HttpRequestBase request)
+            => request.GetOwinContext().Get<IClient>(OwinKeys.StormpathClient);
+
+        public static IAccount GetStormpathAccount(this HttpRequestBase request)
+            => request.GetOwinContext().Get<IAccount>(OwinKeys.StormpathUser);
+    }
+}

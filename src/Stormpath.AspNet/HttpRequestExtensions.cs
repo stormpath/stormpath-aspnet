0e17726701e6c7003ffe459566744e12b08a8fdb
Nate Barbettini <nathanaelb@gmail.com>
2016-05-06 13:44:47 -0700

    Add GetStormpathApplication helper method

--- src/Stormpath.AspNet/HttpRequestExtensions.cs
+++ src/Stormpath.AspNet/HttpRequestExtensions.cs
@@ -1,8 +1,11 @@
 ﻿using System.Net.Http;
 using System.Web;
+using Stormpath.Configuration.Abstractions.Immutable;
 using Stormpath.Owin.Abstractions;
 using Stormpath.SDK.Account;
+using Stormpath.SDK.Application;
 using Stormpath.SDK.Client;
+using Stormpath.SDK.Sync;
 
 namespace Stormpath.AspNet
 {
@@ -19,5 +22,27 @@ namespace Stormpath.AspNet
 
         public static IAccount GetStormpathAccount(this HttpRequestMessage request)
             => request.GetOwinContext().Get<IAccount>(OwinKeys.StormpathUser);
+
+        public static StormpathConfiguration GetStormpathConfiguration(this HttpRequestBase request)
+            => request.GetOwinContext().Get<StormpathConfiguration>(OwinKeys.StormpathConfiguration);
+
+        public static StormpathConfiguration GetStormpathConfiguration(this HttpRequestMessage request)
+            => request.GetOwinContext().Get<StormpathConfiguration>(OwinKeys.StormpathConfiguration);
+
+        public static IApplication GetStormpathApplication(this HttpRequestBase request)
+        {
+            var client = request.GetStormpathClient();
+            var configuration = request.GetStormpathConfiguration();
+
+            return client.GetApplication(configuration.Application.Href);
+        }
+
+        public static IApplication GetStormpathApplication(this HttpRequestMessage request)
+        {
+            var client = request.GetStormpathClient();
+            var configuration = request.GetStormpathConfiguration();
+
+            return client.GetApplication(configuration.Application.Href);
+        }
     }
 }

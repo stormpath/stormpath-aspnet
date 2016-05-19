bfb1f48000825fb0f567545e6d893ad7eb0fa51c
Nate Barbettini <nathanaelb@gmail.com>
2016-05-19 16:00:36 -0700

    Add more doc example code

---
+++ docs/Stormpath.AspNet.DocExamples/Startup_ViewTemplates.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Owin;
+using Stormpath.Configuration.Abstractions;
+
+namespace Stormpath.AspNet.DocExamples
+{
+    public class Startup_ViewTemplates
+    {
+        public void Configuration_CustomView(IAppBuilder app)
+        {
+            #region code/templates/aspnet/custom_view.cs
+            app.UseStormpath(new StormpathConfiguration
+            {
+                Web = new WebConfiguration
+                {
+                    Login = new WebLoginRouteConfiguration
+                    {
+                        View = "~/Views/Login/MyLogin.cshtml"
+                    }
+                }
+            });
+            #endregion
+        }
+    }
+}

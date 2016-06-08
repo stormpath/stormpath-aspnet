c26ef7466a2b0a73d40f066a5563f6ff36d3ed11
Nate Barbettini <nathanaelb@gmail.com>
2016-06-08 08:24:29 -0700

    Add options class

--- src/Stormpath.AspNet/StormpathAppBuilderExtensions.cs
+++ src/Stormpath.AspNet/StormpathAppBuilderExtensions.cs
@@ -1,25 +1,41 @@
 ﻿using System;
-using System.Text;
 using Microsoft.Owin.Extensions;
 using Owin;
+using Stormpath.Configuration.Abstractions;
 using Stormpath.Owin.Middleware;
 
 namespace Stormpath.AspNet
 {
     public static class StormpathAppBuilderExtensions
     {
-        public static IAppBuilder UseStormpath(this IAppBuilder app, object configuration = null)
+        public static IAppBuilder UseStormpath(this IAppBuilder app)
+            => app.UseStormpath(new StormpathMiddlewareOptions());
+
+        public static IAppBuilder UseStormpath(this IAppBuilder app, StormpathConfiguration configuration)
+            => app.UseStormpath(new StormpathMiddlewareOptions()
+            {
+                Configuration = configuration
+            });
+
+        public static IAppBuilder UseStormpath(this IAppBuilder app, object anonymousConfiguration)
+            => app.UseStormpath(new StormpathMiddlewareOptions()
+            {
+                Configuration = anonymousConfiguration
+            });
+
+        public static IAppBuilder UseStormpath(this IAppBuilder app, StormpathMiddlewareOptions options)
         {
             if (app == null)
             {
                 throw new ArgumentNullException(nameof(app));
             }
 
-            var stormpathMiddleware = StormpathMiddleware.Create(new StormpathMiddlewareOptions()
+            var stormpathMiddleware = StormpathMiddleware.Create(new Owin.Middleware.StormpathMiddlewareOptions()
             {
-                LibraryUserAgent = GetLibraryUserAgent(app),
-                Configuration = configuration,
-                ViewRenderer = new SimpleViewRenderer()
+                LibraryUserAgent = GetLibraryUserAgent(),
+                ViewRenderer = new SimpleViewRenderer(),
+                Configuration = options?.Configuration,
+                Logger = options?.Logger,
             });
 
             app.Use(stormpathMiddleware);
@@ -32,12 +48,12 @@ namespace Stormpath.AspNet
             return app;
         }
 
-        private static string GetLibraryUserAgent(IAppBuilder app)
+        private static string GetLibraryUserAgent()
         {
             var libraryVersion = typeof(StormpathMiddleware).Assembly.GetName().Version;
             var libraryToken = $"stormpath-aspnet/{libraryVersion.Major}.{libraryVersion.Minor}.{libraryVersion.Build}";
 
-            var hostToken = $"aspnet/{Environment.Version.ToString()}";
+            var hostToken = $"aspnet/{Environment.Version}";
 
             return string.Join(" ", libraryToken, hostToken);
         }
---
+++ src/Stormpath.AspNet/StormpathMiddlewareOptions.cs
@@ -0,0 +1,11 @@
+﻿using Stormpath.SDK.Logging;
+
+namespace Stormpath.AspNet
+{
+    public sealed class StormpathMiddlewareOptions
+    {
+        public object Configuration { get; set; }
+
+        public ILogger Logger { get; set; }
+    }
+}

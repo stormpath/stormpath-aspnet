0bac586cc8ccb890c957b1af91c6161da7972a59
Nate Barbettini <nathanaelb@gmail.com>
2016-06-08 08:55:36 -0700

    Add logging example to test project

--- test/Stormpath.AspNet.TckHarness/Startup.Auth.cs
+++ test/Stormpath.AspNet.TckHarness/Startup.Auth.cs
@@ -1,4 +1,8 @@
-﻿using Owin;
+﻿using System;
+using System.IO;
+using System.Text;
+using Owin;
+using Stormpath.SDK.Logging;
 
 namespace Stormpath.AspNet.TckHarness
 {
@@ -6,13 +10,61 @@ namespace Stormpath.AspNet.TckHarness
     {
         public void ConfigureAuth(IAppBuilder app)
         {
-            app.UseStormpath(new
+            var stormpathConfig = new
             {
                 application = new
                 {
                     name = "My Application"
                 }
+            };
+
+            var logPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "StormpathMiddleware.log");
+
+            app.UseStormpath(new StormpathMiddlewareOptions()
+            {
+                Configuration = stormpathConfig,
+                Logger = new FileLogger(logPath, LogLevel.Trace)
             });
         }
     }
+
+    public class FileLogger : ILogger
+    {
+        private readonly string _path;
+        private readonly LogLevel _severity;
+
+        public FileLogger(string path, LogLevel severity)
+        {
+            _path = path;
+            _severity = severity;
+        }
+
+        public void Log(LogEntry entry)
+        {
+            if (entry.Severity < _severity)
+            {
+                return;
+            }
+
+            var logBuilder = new StringBuilder()
+                .Append($"[{entry.Severity}] {entry.Source}: ");
+
+            if (entry.Exception != null)
+            {
+                logBuilder.Append($"Exception {entry.Exception.GetType().Name} \"{entry.Exception.Message}\" in {entry.Exception.Source} ");
+            }
+
+            bool isMessageUseful = !string.IsNullOrEmpty(entry.Message)
+                                   && !entry.Message.Equals(entry.Exception?.Message, StringComparison.Ordinal);
+            if (isMessageUseful)
+            {
+                logBuilder.Append($"\"{entry.Message}\"");
+            }
+
+            logBuilder.Append("\n");
+
+            File.AppendAllText(_path, logBuilder.ToString());
+        }
+    }
 }
+

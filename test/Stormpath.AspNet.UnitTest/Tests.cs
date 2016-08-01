b5ca118cd9b82ae122e0e7f8b1add0de8659d7ac
Nate Barbettini <nathanaelb@gmail.com>
2016-08-01 14:19:06 -0700

    Try mocking Account on request

--- test/Stormpath.AspNet.UnitTest/Tests.cs
+++ test/Stormpath.AspNet.UnitTest/Tests.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using FluentAssertions;
+using Moq;
+using Stormpath.Owin.Abstractions;
+using Stormpath.SDK.Account;
 using Xunit;
 
 namespace Stormpath.AspNet.UnitTest
@@ -8,7 +13,28 @@ namespace Stormpath.AspNet.UnitTest
         [Fact]
         public void Test1() 
         {
-            Assert.True(true);
+            var request = new System.Net.Http.HttpRequestMessage();
+
+            var account = new Mock<IAccount>();
+            account.Setup(a => a.Email).Returns("test@example.com");
+
+            request.SetStormpathAccount(account.Object);
+
+            request.GetStormpathAccount().Should().NotBeNull();
+        }
+    }
+
+    public static class RequestExtensions
+    {
+        public static void SetStormpathAccount(this HttpRequestMessage request, IAccount account)
+        {
+            if (request.GetOwinContext() == null)
+            {
+                request.SetOwinEnvironment(new Dictionary<string, object>());
+            }
+
+            request.GetOwinContext().Set(OwinKeys.StormpathUser, account);
+            request.GetOwinContext().Set(OwinKeys.StormpathClient, account.Client);
         }
     }
 }

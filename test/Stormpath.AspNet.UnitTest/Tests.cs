8d4f2236bbaf4307bc5319cd17760dece79b68f2
Nate Barbettini <nathanaelb@gmail.com>
2016-08-22 11:20:50 -0700

    Testing the testing

--- test/Stormpath.AspNet.UnitTest/Tests.cs
+++ test/Stormpath.AspNet.UnitTest/Tests.cs
@@ -23,18 +23,4 @@ namespace Stormpath.AspNet.UnitTest
             request.GetStormpathAccount().Should().NotBeNull();
         }
     }
-
-    public static class RequestExtensions
-    {
-        public static void SetStormpathAccount(this HttpRequestMessage request, IAccount account)
-        {
-            if (request.GetOwinContext() == null)
-            {
-                request.SetOwinEnvironment(new Dictionary<string, object>());
-            }
-
-            request.GetOwinContext().Set(OwinKeys.StormpathUser, account);
-            request.GetOwinContext().Set(OwinKeys.StormpathClient, account.Client);
-        }
-    }
 }

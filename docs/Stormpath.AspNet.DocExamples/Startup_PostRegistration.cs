a57528f834792b7025dfb2ddac79acfde5ce6b6a
Nate Barbettini <nathanaelb@gmail.com>
2016-12-09 10:55:35 -0800

    Add pre/post registration examples

---
+++ docs/Stormpath.AspNet.DocExamples/Startup_PostRegistration.cs
@@ -0,0 +1,64 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Owin;
+using Stormpath.Configuration.Abstractions;
+using Stormpath.Owin.Middleware;
+
+namespace Stormpath.AspNet.DocExamples
+{
+    public class Startup_PostRegistration
+    {
+        public void Configuration_Basic(IAppBuilder app)
+        {
+            #region code/registration/aspnet/postregistration_handler.cs
+            app.UseStormpath(new StormpathMiddlewareOptions()
+            {
+                Configuration = new StormpathConfiguration(), // existing config, if any
+                PostRegistrationHandler = (context, ct) =>
+                {
+                    return Task.FromResult(0);
+                }
+            });
+            #endregion
+        }
+
+        #region code/registration/postregistration_handler_method.cs
+        private Task MyPostRegistrationHandler(
+            PreRegistrationContext context,
+            CancellationToken ct)
+        {
+            return Task.FromResult(0);
+        }
+        #endregion
+
+        public void Configuration_SaveCustomData(IAppBuilder app)
+        {
+            app.UseStormpath(new StormpathMiddlewareOptions()
+            {
+                Configuration = new StormpathConfiguration(), // existing config, if any
+                #region code/registration/postregistration_save_data.cs
+                PostRegistrationHandler = async (ctx, ct) =>
+                {
+                    ctx.Account.CustomData["quota"] = 100;
+                    await ctx.Account.SaveAsync(ct);
+                }
+                #endregion
+            });
+        }
+
+        public void Configuration_AddGroup(IAppBuilder app)
+        {
+            app.UseStormpath(new StormpathMiddlewareOptions()
+            {
+                Configuration = new StormpathConfiguration(), // existing config, if any
+                #region code/registration/postregistration_add_group.cs
+                PostRegistrationHandler = async (ctx, ct) =>
+                {
+                    await ctx.Account.AddGroupAsync("customers", ct);
+                }
+                #endregion
+            });
+        }
+    }
+}
+
---
+++ docs/Stormpath.AspNet.DocExamples/Startup_PreRegistration.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Owin;
+using Stormpath.Configuration.Abstractions;
+using Stormpath.Owin.Middleware;
+
+namespace Stormpath.AspNet.DocExamples
+{
+    public class Startup_PreRegistration
+    {
+        public void Configuration_Basic(IAppBuilder app)
+        {
+            #region code/registration/aspnet/preregistration_handler.cs
+            app.UseStormpath(new StormpathMiddlewareOptions()
+            {
+                Configuration = new StormpathConfiguration(), // existing config, if any
+                PreRegistrationHandler = (context, ct) =>
+                {
+                    return Task.FromResult(0);
+                }
+            });
+            #endregion
+        }
+
+        #region code/registration/preregistration_handler_method.cs
+        private Task MyPreRegistrationHandler(
+            PreRegistrationContext context,
+            CancellationToken ct)
+        {
+            return Task.FromResult(0);
+        }
+        #endregion
+
+        public void Configuration_TargetDirectory(IAppBuilder app)
+        {
+            app.UseStormpath(new StormpathMiddlewareOptions()
+            {
+                Configuration = new StormpathConfiguration(), // existing config, if any
+                #region code/registration/preregistration_target_dir.cs
+                PreRegistrationHandler = async (ctx, ct) =>
+                {
+                    ctx.AccountStore = await ctx.Client.GetDirectoryAsync(
+                        "https://api.stormpath.com/v1/directories/xxx", ct);
+                }
+                #endregion
+            });
+        }
+    }
+}
+
